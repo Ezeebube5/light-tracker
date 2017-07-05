9cafb5c587b10b179c49ec49e7826c71f0a5ee77
Eze EBube <Ezeebube5@gmail.com>
2017-07-05 16:15:44 +0100

    Added FAB and cards

--- src/Containers/HomeScreen.js
+++ src/Containers/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Button, Card, CardItem, Fab, Text, Body } from 'native-base';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 // Add Actions - replace 'Your' with whatever your reducer is called :)
@@ -9,13 +10,57 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import styles from './Styles/LoadingScreenStyle';
 
 class HomeScreen extends React.Component {
+ //  constructor() {
+ //   this.state = {
+ //     active: 'true'
+ //   };
+ // }
   static navigationOptions = {
    tabBarLabel: 'home',
    tabBarIcon: ({ tintColor }) => <Icon size={30} name={'home'} color={tintColor} />
  }
+
   render() {
     return (
-      <View style={styles.container} />
+      <View style={{flex: 1}}>
+        <Card>
+          <CardItem header>
+            <Text>Personal Health</Text>
+          </CardItem>
+          <CardItem>
+            <Body>
+              <Text> Weight </Text>
+              <Text> BMI </Text>
+            </Body>
+          </CardItem>
+        </Card>
+
+        <Card>
+          <CardItem header>
+            <Text> Fitness Statistics</Text>
+          </CardItem>
+          <CardItem>
+            <Body>
+              <Text> Daily Steps </Text>
+              <Text> Distance Travelled </Text>
+              <Text> Calorieis Burnt </Text>
+              <Text> Badge </Text>
+
+            </Body>
+          </CardItem>
+        </Card>
+
+        <Fab
+//          active={this.state.active}
+          direction="up"
+          containerStyle={{ }}
+          style={{ backgroundColor: '#424242' }}
+          position="bottomRight"
+    //      onPress={() => this.setState({ active: !this.state.active })}
+        >
+          <Icon name="user" />
+        </Fab>
+      </View>
     );
   }
 }
